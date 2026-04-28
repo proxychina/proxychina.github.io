b373bf35024d4c33371e91d082a9bc66ed715700
jiu.pins@outlook.com <jiu.pins@outlook.com>
2026-04-28 21:12:11 +0800

    le hen duo gen zhe zi ji de jie zou zuo chan pin man man da mo ti yan nan dao bu xiang ma zi

--- assets/website/js/frontend/base.js
+++ assets/website/js/frontend/base.js
@@ -16,7 +16,15 @@
                 var that = $(this);
                 that.parent('td').find('span').text(that.data('text'));
                 that.hide();
-            })
+            });
+
+            const currentDate = new Date();
+            const year = currentDate.getFullYear();
+            const month = currentDate.getMonth() + 1; // 月份从0开始，需要加1
+            const day = currentDate.getDate();
+
+            $('#js-ad_date').text(year + "-" + month + "-" + day);
+            $('#js-ad_href').attr('href', 'https://www.freeclashnode.com');
         }
     };
 
